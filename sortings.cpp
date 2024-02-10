1558610be3ba2f96335f103a1b9e2a5b35018ef2
Mihir Moolchandani <121425409+horrible-hacker@users.noreply.github.com>
2024-02-10 21:44:23 +0530

    Update sortings.cpp
    added comments + added insertion sort and some other functions

--- sortings.cpp
+++ sortings.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
 using namespace std;
-void swap(int *a , int *b)
+void swap(int *a , int *b) //swap 2 numbers
 {
   int temp;
   temp=*a;
   *a=*b;
   *b=temp;
 }
-void selection_sort(int a[],int l)
+void duplicate(int a[],int b[],int n) //put the values on fulled array a into array b
+{
+  for(int i=0;i<n;i++)
+  {
+    b[i]=a[i];
+  }
+}
+
+void show (int a[],int n) //display the array
+{
+  for(int i=0;i<n;i++)
+  {
+    cout<<a[i]<<" ";
+  }
+  cout<<endl;
+}
+void selection_sort(int a[],int l) //using selction sort which take O(n^2) time 
 {
       for(int i=0;i<l-1;i++)
       {
@@ -19,10 +35,21 @@ void selection_sort(int a[],int l)
             }
         }
       }
-      for(int i=0;i<l;i++)
+      show(a,l);
+}
+void insertion_sort(int a[],int l) //using insertion sort 
+{
+  for(int i=0;i<l-1;i++)
+  {
+    for(int j=0;j<l-i-1;j++)
+    {
+      if(a[j]>a[j+1])
       {
-        cout<<a[i]<<" ";
+          swap(a[j],a[j+1]);
       }
+    }
+  }
+  show(a,l);
 }
 int main()
 {
@@ -33,6 +60,11 @@ int main()
   {
     cin>>a[i];
   }
-  selection_sort(a,n);
+  int b[n],c[n];
+  duplicate(a,b,n);
+  duplicate(a,c,n);
+  selection_sort(b,n);
+  insertion_sort(c,n);
+  show(a,n);
   return 0;
 }
